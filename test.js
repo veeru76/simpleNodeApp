119f23646fd337691fa2efc08db7d4d80fd1ef4b
veeru76 <54212169+veeru76@users.noreply.github.com>
2026-03-29 10:57:00 +0530

    enable docker hub push

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ console.log('Running tests...');
 
 // A very simple test scenario
 try {
-  assert.strictEqual(1 + 1, 3, 'Math is broken!');
+  assert.strictEqual(1 + 1, 2, 'Math is broken!');
   console.log('All tests passed!');
   process.exit(0);
 } catch (error) {
